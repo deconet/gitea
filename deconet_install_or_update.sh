7e3e2106489dc99f4965bcbaa36122f018b05bdd
Chris Cassano <glitch0@gmail.com>
2018-05-23 19:27:35 -0700

    remove old templates

--- deconet_install_or_update.sh
+++ deconet_install_or_update.sh
@@ -2,4 +2,5 @@
 
 cp dist/binaries/gitea-master-linux-amd64 /home/git/
 mkdir -p /home/git/custom
-cp -R custom/templates /home/git/custom/templates
+rm -rf /home/git/custom/templates # clear out old templates
+cp -R custom/templates /home/git/custom/templates # copy new templates
